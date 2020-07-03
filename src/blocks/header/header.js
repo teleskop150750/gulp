3a2ff9dec946f971d42c42e944a121172f97ad80
Teleskop 150750 <teleskop150750@gmail.com>
2020-07-03 20:34:30 +0300

    формат js

--- src/blocks/header/header.js
+++ src/blocks/header/header.js
@@ -3,6 +3,6 @@
 (() => {
     const a = 4;
     const b = 4;
-    let result = a + b;
+    const result = a + b;
     console.log(result);
 })();
--- src/js/libs/webp.js
+++ src/js/libs/webp.js
@@ -2,14 +2,13 @@
 
 (() => {
   const webP = new Image();
-  webP.src =
-    "data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA";
+  webP.src = 'data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA';
 
   webP.onerror = () => {
     if (webP.height === 2) {
-      document.querySelector("body").classList.add("webp");
+      document.querySelector('body').classList.add('webp');
     } else {
-      document.querySelector("body").classList.add("no-webp");
+      document.querySelector('body').classList.add('no-webp');
     }
   };
 
