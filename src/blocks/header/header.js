3552ed3daf3765228bd95330f8e67c758c7db45a
teleskop 150750 <teleskop150750@gmail.com>
2020-07-09 16:52:38 +0300

    форматирование файлов

--- src/blocks/header/header.js
+++ src/blocks/header/header.js
@@ -1,8 +1,8 @@
 // header
 
 (() => {
-    const a = 4;
-    const b = 4;
-    const result = a + b;
-    console.log(result);
+	const a = 4;
+	const b = 4;
+	const result = a + b;
+	console.log(result);
 })();
