f3555a0525a0cdf614b2787dad96377f09f69103
teleskop 150750 <teleskop150750@gmail.com>
2020-08-11 22:02:21 +0300

    ''

--- src/blocks/header/header.js
+++ src/blocks/header/header.js
@@ -136,3 +136,31 @@
 
   // render(content[linkActiveHref]);
 })();
+
+(() => {
+  const button = document.querySelector('.header__burger');
+  const nav = document.querySelector('.header__nav');
+  const body = document.querySelector('.page__body');
+
+  const closeNav = (e) => {
+    e.stopPropagation();
+    if (!(!!e.target.closest('.header__nav')
+      || e.target.classList.contains('header__nav'))) {
+      nav.classList.remove('header__nav--open');
+      body.classList.remove('page__body--overflow');
+    }
+  };
+
+  const openNav = (e) => {
+    e.stopPropagation();
+    nav.classList.toggle('header__nav--open');
+    body.classList.toggle('page__body--overflow');
+    if (nav.classList.contains('header__nav--open')) {
+      body.addEventListener('click', closeNav);
+    } else {
+      body.removeEventListener('click', closeNav);
+    }
+  };
+
+  button.addEventListener('click', openNav);
+})();
