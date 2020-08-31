b7f41b020c1a0f0ec1647b7fc4410c6fba3c346e
teleskop150750 <teleskop150750@gmail.com>
2020-08-31 23:11:05 +0300

    eslint

--- src/js/global/webp.js
+++ src/js/global/webp.js
@@ -2,7 +2,6 @@
 
 (() => {
   const body = document.documentElement;
-  console.log(body);
   body.classList.remove('no-webp');
   const webP = new Image();
   webP.src = 'data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA';
