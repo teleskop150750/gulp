1a09ecfd8358817a789fb2de0355f016a6295363
teleskop150750 <teleskop150750@gmail.com>
2020-09-03 01:28:58 +0300

    log

--- src/js/global/padding-scroll.js
+++ src/js/global/padding-scroll.js
@@ -1,39 +1,18 @@
 /* eslint-disable no-param-reassign */
+
 export default () => ({
   addPadding() {
     const elements = document.querySelectorAll('.js-scroll');
-    console.log(elements);
     elements.forEach((item) => {
       const { paddingRight } = getComputedStyle(item);
-      console.log('add');
-      console.log(paddingRight);
       item.style.paddingRight = `${parseFloat(paddingRight) + window.innerWidth - document.documentElement.clientWidth}px`;
-      console.log(parseFloat(paddingRight), 'parseFloat(paddingRight');
-      console.log(window.innerWidth, 'ширина 1');
-      console.log(document.documentElement.clientWidth, 'ширина 2');
-      console.log(
-        parseFloat(paddingRight)
-        + window.innerWidth
-        - document.documentElement.clientWidth, 'ширина pad',
-      );
     });
   },
   removePadding() {
     const elements = document.querySelectorAll('.js-scroll');
-    console.log(elements);
     elements.forEach((item) => {
       const { paddingRight } = getComputedStyle(item);
-      console.log('remove');
-      console.log(paddingRight, 'padding css');
       item.style.paddingRight = `${parseFloat(paddingRight) - (window.innerWidth - document.documentElement.clientWidth)}px`;
-      console.log(parseFloat(paddingRight), 'parseFloat(paddingRight');
-      console.log(window.innerWidth, 'ширина 1');
-      console.log(document.documentElement.clientWidth, 'ширина 2');
-      console.log(
-        parseFloat(paddingRight)
-        - (window.innerWidth
-        - document.documentElement.clientWidth), 'ширина pad',
-      );
     });
   },
 });
