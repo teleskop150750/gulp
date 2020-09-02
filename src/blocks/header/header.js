fdc630bebc63bed1816469baa001ba9f49ddacd0
teleskop150750 <teleskop150750@gmail.com>
2020-09-03 01:27:48 +0300

    scroll js

--- src/blocks/header/header.js
+++ src/blocks/header/header.js
@@ -1,6 +1,9 @@
 // header
 
+import paddingScroll from '../../js/global/padding-scroll';
+
 export default () => {
+  const padding = paddingScroll();
   const button = document.querySelector('.header__burger');
   const nav = document.querySelector('.header__nav');
   const body = document.querySelector('.page__body');
@@ -10,19 +13,24 @@ export default () => {
     if (!(e.target.classList.contains('header__nav') || !!e.target.closest('.header__nav'))) {
       nav.classList.remove('header__nav--open');
       body.classList.remove('page__body--overflow');
+      padding.removePadding();
     }
+    body.removeEventListener('click', closeNav);
   };
 
-  const openNav = (e) => {
+  const toggleNav = (e) => {
     e.stopPropagation();
     nav.classList.toggle('header__nav--open');
-    body.classList.toggle('page__body--overflow');
     if (nav.classList.contains('header__nav--open')) {
+      padding.addPadding();
+      body.classList.add('page__body--overflow');
       body.addEventListener('click', closeNav);
     } else {
+      body.classList.remove('page__body--overflow');
+      padding.removePadding();
       body.removeEventListener('click', closeNav);
     }
   };
 
-  button.addEventListener('click', openNav);
+  button.addEventListener('click', toggleNav);
 };
