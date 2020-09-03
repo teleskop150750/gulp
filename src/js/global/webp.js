b4551a3e5a1b1aaad823911afdb3b5af70631e1f
teleskop150750 <teleskop150750@gmail.com>
2020-09-03 04:59:16 +0300

    webp

--- src/js/global/webp.js
+++ src/js/global/webp.js
@@ -5,12 +5,15 @@ export default () => {
   const webP = new Image();
   webP.src = 'data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA';
 
-  webP.onerror = () => {
+  webP.onload = () => {
     if (webP.height === 2) {
-      html.classList.remove('no-webp');
       html.classList.add('webp');
+    } else {
+      html.classList.add('no-webp');
     }
   };
 
-  webP.onload = webP.onerror;
+  webP.onerror = () => {
+    html.classList.add('no-webp');
+  };
 };
